1d5df684ffca8d00ad6e71baf955b8bf3c30aab8
Nikita Melkozerov <n.melkozerov@gmail.com>
2020-07-11 00:48:00 +0200

    repl!

--- src/object.rs
+++ src/object.rs
@@ -1,6 +1,20 @@
+use std::fmt::{Display, Formatter};
+use core::fmt;
+
 #[derive(Debug)]
 pub enum Object {
     Integer(i64),
     Boolean(bool),
     Null,
+}
+
+use crate::object::Object::*;
+impl Display for Object {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Integer(int) => write!(f, "{}", int),
+            Boolean(b) => write!(f, "{}", b),
+            Null => write!(f, "null"),
+        }
+    }
 }
--- src/repl.rs
+++ src/repl.rs
@@ -1,6 +1,8 @@
 use crate::lexer::lexer::Lexer;
 use crate::parser::Parser;
 use std::io;
+use crate::evaluator;
+use crate::ast::Node::Prog;
 
 static PROMT: &str = ">> ";
 
@@ -17,7 +19,10 @@ pub fn start() {
                 let lexer = Lexer::new(&buffer);
                 let parser = Parser::new(lexer);
                 match parser.parse_program() {
-                    Ok(program) => println!("{}", program),
+                    Ok(program) => match evaluator::eval(Prog(program)) {
+                        Ok(obj) => println!("{}", obj),
+                        Err(err) => println!("evaluation error {}", err)
+                    },
                     Err(err) => println!("Parsing error: {}", err.join(", ")),
                 };
             }
