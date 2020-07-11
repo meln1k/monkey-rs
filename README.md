86998ea36ba1a974d7d08eb934e73bb0288b1670
Nikita Melkozerov <n.melkozerov@gmail.com>
2020-07-11 14:03:25 +0200

    evaluation of if/else expressions

---
+++ README.md
@@ -0,0 +1,7 @@
+# Monkey Language Interpreter
+
+An interpreter from the [Writing An Interpreter In Go](https://interpreterbook.com/) book written in Rust.
+
+Features:
+ - Full utf-8 support
+ - Strong typing (no implicit type conversions)
--- src/evaluator.rs
+++ src/evaluator.rs
@@ -3,20 +3,21 @@ use crate::ast::*;
 use crate::lexer::lexer::Lexer;
 use crate::object::{Object, FALSE, TRUE};
 use crate::parser::Parser;
+use crate::ast::Statement::Block;
 
 type EvalResult = Result<Object, String>;
 
 pub fn eval(node: Node) -> EvalResult {
     match node {
-        Node::Prog(program) => eval_program(program),
+        Node::Prog(Program{ statements}) => eval_statements(statements),
         other => todo!(),
     }
 }
 
-fn eval_program(program: Program) -> EvalResult {
+fn eval_statements(statements: Vec<Statement>) -> EvalResult {
     let mut obj: Option<Object> = None;
 
-    for statement in program.statements {
+    for statement in statements {
         obj = Some(eval_statement(statement)?);
     }
 
@@ -26,6 +27,7 @@ fn eval_program(program: Program) -> EvalResult {
 fn eval_statement(statement: Statement) -> EvalResult {
     match statement {
         Statement::Expr(ExpressionStatement { expression }) => eval_expression(expression),
+        Statement::Block(BlockStatement{statements}) => eval_statements(statements),
         _ => todo!(),
     }
 }
@@ -47,9 +49,21 @@ fn eval_expression(expression: Expression) -> EvalResult {
             let right = eval_expression(*right)?;
             eval_infix_expression(operator, left, right)
         }
+        Expression::IfExpression { condition, consequence, alternative} => {
+            let cond = eval_expression(*condition)?;
+            match cond {
+                TRUE => eval_statement(Block(*consequence)),
+                FALSE => match alternative {
+                    Some(block) => eval_statement(Block(*block)),
+                    None => Ok(Object::Null)
+                },
+                other => Err(format!("if expression can only use bool as a condition, but got {}", other)),
+            }
+        }
         _ => todo!(),
     }
 }
+
 
 fn eval_prefix_expression(operator: PrefixOperator, right: Object) -> EvalResult {
     match operator {
@@ -174,6 +188,30 @@ fn test_bang_operator() {
     for Test(input, expected) in tests {
         let evaluated = test_eval(input).expect("evaluation failed");
         test_boolean_object(evaluated, expected);
+    }
+}
+
+#[test]
+fn test_if_else_expressions() {
+    struct Test<'a>(&'a str, Option<i64>);
+
+    let tests = vec![
+        Test("if (true) { 10 }", Some(10)),
+        Test("if (false) { 10 }", None),
+        Test("if (1 < 2) { 10 }", Some(10)),
+        Test("if (1 > 2) { 10 }", None),
+        Test("if (1 > 2) { 10 } else { 20 }", Some(20)),
+        Test("if (1 < 2) { 10 } else { 20 }", Some(10)),
+    ];
+
+    for Test(input, expected) in tests {
+        let evaluated = test_eval(input).expect("evaluation failed");
+
+        match expected {
+            Some(int) => test_integer_object(evaluated, int),
+            None => assert_eq!(evaluated, Object::Null)
+        }
+
     }
 }
 
